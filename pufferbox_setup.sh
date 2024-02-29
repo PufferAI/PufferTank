aa3207ea3c216c5e94ce3a09322e57eb1cb65ac3
Joseph Suarez <jsuarez@puffer.ai>
2024-02-29 17:47:38 +0000

    Pufferbox setup

---
+++ pufferbox_setup.sh
@@ -0,0 +1,8 @@
+sudo apt update
+
+# Docker
+sudo apt install apt-transport-https ca-certificates curl gnupg
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
