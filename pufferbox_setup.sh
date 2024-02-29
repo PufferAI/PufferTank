5783b361be69d2f7d7f794eba2ee1818ae1cb79f
Joseph Suarez <jsuarez@puffer.ai>
2024-02-29 18:03:27 +0000

    Remove sudo

--- pufferbox_setup.sh
+++ pufferbox_setup.sh
@@ -1,8 +1,8 @@
-sudo apt update
+apt update
 
 # Docker
-sudo apt install apt-transport-https ca-certificates curl gnupg
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
+apt install apt-transport-https ca-certificates curl gnupg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
