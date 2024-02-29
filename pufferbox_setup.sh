00c2b083671349f0806897dfaf7d699ed0338fb6
Joseph Suarez <jsuarez@puffer.ai>
2024-02-29 23:12:12 +0000

    Update puffer setup

--- pufferbox_setup.sh
+++ pufferbox_setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Run this script as root. Prerequisites:
-#   1. Disable secure boot in BIOS settings! Otherwise, NVIDIA drivers will not work.
-#   2. apt-get update && apt-get install -y git
-#   3. cd /home/puffer && git clone https://github.com/pufferai/puffertank
-
 # Update packages
 apt-get update -y
 
@@ -20,63 +15,71 @@ apt-get install -y \
     curl \
     gnupg
 
-# Add unstable and experimental repositories. Required for NVIDIA drivers and
+# Function to add a repo if not already present in /etc/apt/sources.list
+add_repo() {
+    REPO_URL="$1"
+    REPO_ENTRY="$2 main contrib non-free"
+    if ! grep -qRFe "$REPO_URL" /etc/apt/; then
+        echo "$REPO_ENTRY" | tee -a /etc/apt/sources.list > /dev/null
+    fi
+}
+
+# Modify /etc/apt/sources.list to include contrib and non-free for existing Debian entries
 sed -i '/^deb .*main/ s/main/main contrib non-free/' /etc/apt/sources.list
-UNSTABLE_REPO="deb http://deb.debian.org/debian/ unstable main contrib non-free"
-EXPERIMENTAL_REPO="deb http://deb.debian.org/debian/ experimental main contrib non-free"
-grep -qxF "$UNSTABLE_REPO" /etc/apt/sources.list || echo "$UNSTABLE_REPO" >> /etc/apt/sources.list
-grep -qxF "$EXPERIMENTAL_REPO" /etc/apt/sources.list || echo "$EXPERIMENTAL_REPO" >> /etc/apt/sources.list
+
+# Add unstable and experimental repositories. Required for NVIDIA drivers
+add_repo "http://deb.debian.org/debian/" "deb http://deb.debian.org/debian/ unstable"
+add_repo "http://deb.debian.org/debian/" "deb http://deb.debian.org/debian/ experimental"
 
 # Install Tailscale
-curl -fsSL https://tailscale.com/install.sh | sh
+if ! command -v tailscale &> /dev/null; then
+    curl -fsSL https://tailscale.com/install.sh | sh
+fi
 
-# Install Docker
+# Docker installation
 DOCKER_GPG_KEY="/usr/share/keyrings/docker.gpg"
 if [ ! -f "$DOCKER_GPG_KEY" ]; then
     curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o $DOCKER_GPG_KEY
 fi
 
+DOCKER_REPO="deb [arch=$(dpkg --print-architecture) signed-by=$DOCKER_GPG_KEY] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 DOCKER_REPO_FILE="/etc/apt/sources.list.d/docker.list"
-if [ ! -f "$DOCKER_REPO_FILE" ]; then
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=$DOCKER_GPG_KEY] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | \
-        tee $DOCKER_REPO_FILE > /dev/null
+if ! grep -qF -- "$DOCKER_REPO" "$DOCKER_REPO_FILE" 2>/dev/null; then
+    echo "$DOCKER_REPO" | tee $DOCKER_REPO_FILE > /dev/null
 fi
 
 apt-get update -y
-
 apt-get install -y \
     docker-ce \
     docker-ce-cli \
     containerd.io \
     docker-buildx-plugin
 
+# Add user to the Docker group
 usermod -aG docker puffer
 
-grep -qxF "cd /home/puffer && bash docker.sh test" /etc/bash.bashrc || \
+# Ensure docker.sh test script is run at login
+if ! grep -qxF "cd /home/puffer && bash docker.sh test" /etc/bash.bashrc; then
     echo "cd /home/puffer && bash docker.sh test" >> /etc/bash.bashrc
-
-docker pull pufferai/puffertank:latest
-
-
-# Install NVIDIA 535 drivers. They require unstable and experimental packages.
-apt-get install -t experimental -y nvidia-driver
-
-# Install container toolkit. Requires Debian11 (bullseye) repository
-NVIDIA_DOCKER_GPG_KEY="/etc/apt/keyrings/nvidia-docker.gpg"
-if [ ! -f "$NVIDIA_DOCKER_GPG_KEY" ]; then
-    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | gpg --dearmor -o $NVIDIA_DOCKER_GPG_KEY
 fi
 
+# Pull the latest Docker image
+docker pull pufferai/puffertank:latest
+
+# Install NVIDIA drivers
+apt-get install -t experimental -y nvidia-driver
+
+# NVIDIA container toolkit setup
+NVIDIA_DOCKER_REPO="deb [signed-by=$NVIDIA_DOCKER_GPG_KEY] https://nvidia.github.io/nvidia-container-runtime/debian11 $(lsb_release -cs) stable"
 NVIDIA_DOCKER_REPO_FILE="/etc/apt/sources.list.d/nvidia-docker.list"
-if [ ! -f "$NVIDIA_DOCKER_REPO_FILE" ]; then
-    echo "deb [signed-by=$NVIDIA_DOCKER_GPG_KEY] https://nvidia.github.io/nvidia-container-runtime/debian11 $(lsb_release -cs) stable" > \
-        $NVIDIA_DOCKER_REPO_FILE
+if ! grep -qF -- "$NVIDIA_DOCKER_REPO" "$NVIDIA_DOCKER_REPO_FILE" 2>/dev/null; then
+    echo "$NVIDIA_DOCKER_REPO" | tee $NVIDIA_DOCKER_REPO_FILE > /dev/null
 fi
 
 apt-get update && apt-get install -y nvidia-container-toolkit
 systemctl restart docker
 
-# Instructions to complete the manual portion of installation 
+# Completion message with instructions
 echo -e "Installation complete.\nTo complete installation:\n\
 1) Set passwords:\n\
    - passwd puffer\n\
