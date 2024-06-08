f26191969504d9d5b864ba5cf978aeae62b21479
Joseph Suarez <jsuarez@puffer.ai>
2024-06-08 00:31:03 -0400

    docker sh

--- docker.sh
+++ docker.sh
@@ -5,7 +5,7 @@ username="pufferai"  # replace with your Docker Hub username
 dockerfile=""  # Dockerfile to use
 image="puffertank"
 tag="1.0"
-name="${image}-${tag}"
+name="puffertank"
 
 # Function for building Docker image
 build() {
